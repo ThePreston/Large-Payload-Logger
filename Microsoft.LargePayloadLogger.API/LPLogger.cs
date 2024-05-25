efcc4f39f98028ac4299224fbaa6e4a2fcdacdfd
Preston Hale <prestonhale@microsoft.com>
2024-05-25 14:27:09 -0500

    Created Separate methods for Claim Check using EventHub and BlobLogger for just savingt to blob.
    also added capabilities for file name convention, it is faster to add headers and use those than it is to deserialize a large payload

--- Microsoft.LargePayloadLogger.API/LPLogger.cs
+++ Microsoft.LargePayloadLogger.API/LPLogger.cs
@@ -4,7 +4,10 @@ using Azure.Storage.Blobs;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Logging;
+using Microsoft.LargePayloadLogger.API.Models;
+using Newtonsoft.Json;
 using System.Text;
 
 namespace Microsoft.LargePayloadLogger.API
@@ -24,20 +27,39 @@ namespace Microsoft.LargePayloadLogger.API
             _eventHubClient = producerClient;
         }
 
-        [Function("LPLogger")]
-        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+        [Function("ClaimCheckLogger")]
+        public async Task<IActionResult> ClaimCheckLogger([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var objPayload = CreatePayloadObject(await new StreamReader(req.Body).ReadToEndAsync(), req.Headers, nameof(ClaimCheckLogger));
 
-            var headerValues = req.Headers.ToList();
-
-            var fileUri = await PersistToStorage($"{Guid.NewGuid()}.txt", $" Headers = {string.Join(';', headerValues.ToArray())}; Body = {requestBody}");
+            var fileUri = await PersistToStorage($"{objPayload.SessionKey}-{objPayload.PayloadType}.json", objPayload.Payload);
 
             await PersistToEventHub($"file uri = {fileUri}");
 
-            return new OkObjectResult("Message Persisted");
+            return new OkObjectResult($"Message Persisted {fileUri}");
+        }
+
+        [Function("BlobLogger")]
+        public async Task<IActionResult> BlobLogger([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var objPayload = CreatePayloadObject(await new StreamReader(req.Body).ReadToEndAsync(), req.Headers, nameof(BlobLogger));
+
+            var fileUri = await PersistToStorage($"{objPayload.SessionKey}-{objPayload.PayloadType}.json", objPayload.Payload);
+
+            return new OkObjectResult($"Message Persisted {fileUri}");
+        }
+
+        private static LargePayload CreatePayloadObject(string body, IHeaderDictionary headers, string defaultType)
+        {
+            
+            return new LargePayload() { Payload = body, 
+                                        SessionKey = headers.ContainsKey("SessionId") ? headers["SessionId"].ToString() :  Guid.NewGuid().ToString(), 
+                                        PayloadType = headers.ContainsKey("PayloadType") ? headers["payloadType"].ToString() : defaultType
+            };
         }
 
         private async Task<string> PersistToStorage(string fileName, string content)
@@ -60,7 +82,6 @@ namespace Microsoft.LargePayloadLogger.API
             return blobClient.Uri.ToString();
         }
 
-
         private async Task PersistToEventHub(string content)
         {
 
@@ -68,7 +89,7 @@ namespace Microsoft.LargePayloadLogger.API
             eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes(content)));
             await _eventHubClient.SendAsync(eventBatch);
 
-            _logger.LogInformation($"Persisted To EventHub ");
+            _logger.LogInformation($"Persisted To EventHub");
 
         }
     }
---
+++ Microsoft.LargePayloadLogger.API/Models/LargePayload.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Microsoft.LargePayloadLogger.API.Models
+{
+    public class LargePayload
+    {
+        [JsonPropertyName("payload")]
+        public string Payload { get; set; } = string.Empty;
+
+        [JsonPropertyName("payloadType")]
+        public string PayloadType { get; set; } = string.Empty;
+
+        [JsonPropertyName("sessionKey")]
+        public string SessionKey { get; set; } = string.Empty;
+
+    }
+}
